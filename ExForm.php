2e75ae1f81c5652625ea778304741d7f4dda2cf9
jollymugivara <59286915+jollymugivara@users.noreply.github.com>
2020-01-09 15:01:19 +0300

    Add files via upload

---
+++ ExForm.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Drupal\ex_form\Form;
+
+use Drupal\Core\Form\FormBase;																			
+use Drupal\Core\Form\FormStateInterface;													
+
+
+class ExForm extends FormBase {
+
+	public function buildForm(array $form, FormStateInterface $form_state) {
+
+		$form['firstname'] = [
+			'#type' => 'textfield',
+			'#title' => $this->t('Firstname'),
+			'#description' => $this->t('Имя не должно содержать цифр'),
+			'#required' => TRUE,
+		];
+
+      $form['lastname'] = [
+			'#type' => 'textfield',
+			'#title' => $this->t('Lastname'),
+			'#description' => $this->t('Фамилия не должна содержать цифр'),
+			'#required' => TRUE,
+		];
+
+    $form['subject'] = [
+			'#type' => 'textfield',
+			'#title' => $this->t('Subject'),
+			'#required' => TRUE,
+		]; 
+
+    $form['message'] = [
+			'#type' => 'textfield',
+			'#title' => $this->t('Message'),
+			'#required' => TRUE,
+		];
+
+    $form['email'] = [
+			'#type' => 'textfield',
+			'#title' => $this->t('Email'),
+			'#required' => TRUE,
+		];
+
+		$form['actions']['submit'] = [
+			'#type' => 'submit',
+			'#value' => $this->t('Submit'),
+		];
+
+    	return $form;
+	}
+   public function validateForm(array &$form, FormStateInterface $form_state) {
+		$title = $form_state->getValue('lastname');
+    $title = $form_state->getValue('firstname');
+		$is_number = preg_match("/[\d]+/", $title, $match);
+
+		if ($is_number > 0) {
+			$form_state->setErrorByName('lastname', $this->t('Строка содержит цифру.'));
+      $form_state->setErrorByName('firstname', $this->t('Строка содержит цифру.'));
+		}
+	}
+
+
+	public function submitForm(array &$form, FormStateInterface $form_state) {
+		drupal_set_message($this->t('Thank you @name,your e-mail is @email', array(
+    $firstname => $form_state ->getValue('firstname'),
+    $lastname => $form_state ->getValue('lastname'),
+    $subject => $form_state ->getValue('subject'),
+    $message => $form_state ->getValue('message'),
+    $email => $form_state ->getValue('email');
+
+
+    $url = "https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/".$email."/?hapikey=0425c9e1-339a-495d-9acf-02363b6f9b8c";
+
+    $data = array(
+      'properties' => [
+        [
+          'property' => 'firstname',
+          'value' => $firstname
+        ],
+        [
+          'property' => 'lastname',
+          'value' => $lastname 
+        ],
+        [
+          'property' => 'subject',
+          'value' => $subject
+        ],
+        [
+          'property' => 'message',
+          'value' => $message 
+        ],
+        [
+          'property' => 'email',
+          'value' => $email
+        ],
+      ]
+    );
+
+
+    $json = json_encode($data,true);
+
+    $response = \Drupal::httpClient()->post($url.'&_format=hal_json', [
+      'headers' => [
+        'Content-Type' => 'application/json'
+      ],
+        'body' => $json
+    ]);
+
+
+	}
+
+}
+
---
+++ Hubspot.php
@@ -0,0 +1,118 @@
+<?php
+
+if( isset($_POST['submit']) )
+{
+
+	$firstname=$_POST['firstname'];
+	$lastname=$_POST['lastname'];
+	$subject=$_POST['subject'];
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+
+
+
+	$contact_data = array(
+		"firstname" => $firstname,
+		"lastname" => $lastname,
+		"email" => $email,
+		"subject" => $subject,
+		"message" => $message,
+
+	);
+	
+	$ans_hubspot = new ans_hubspot();
+	$ans_hubspot->contact_create($contact_data);
+	$ans_hubspot->list_assign_contact("2", $contact_data["email"]);
+    
+
+}
+
+class ans_hubspot{
+	private $hapikey = "0425c9e1-339a-495d-9acf-02363b6f9b8c";
+
+	function list_assign_contact($lid, $email){
+		(object)$arr = array(
+			"email" => array($email)
+		);
+        $post_json = json_encode($arr);
+        $endpoint = 'https://api.hubapi.com/contacts/v1/lists/'.$lid.'/add?hapikey=' . $this->hapikey;
+		$this->http($endpoint,$post_json);
+	}	
+	
+	
+	function list_create($list_name){
+		$arr = array(
+		    "name" => $list_name,
+		    "dynamic" => false,
+		    "filters" => array(
+		    	array(
+		    		(object)array(
+						"operator" => "EQ",
+						"value" => "@hubspot",
+						"property" => "twitterhandle",
+						"type" => "string"
+					)
+            	)  
+		    )
+		);
+        $post_json = json_encode($arr);
+        $endpoint = 'https://api.hubapi.com/contacts/v1/lists?hapikey=' . $this->hapikey;
+		$this->http($endpoint,$post_json);
+	}
+
+	function contact_create($contact_data){
+     $arr = array(
+        'properties' => array(
+            array(
+                'property' => 'firstname',
+                'value' => $contact_data["firstname"]
+            ),
+			      array(
+                'property' => 'lastname',
+                'value' => $contact_data["lastname"]
+            ),
+            array(
+                'property' => 'subject',
+                'value' => $contact_data["subject"]
+            ),
+            array(
+                'property' => 'message',
+                'value' => $contact_data["message"]
+            ),
+            array(
+                'property' => 'email',
+                'value' => $contact_data["email"]
+            ),
+	
+            )
+            
+        )
+    );
+
+        $post_json = json_encode($arr);
+        $endpoint = 'https://api.hubapi.com/contacts/v1/contact?hapikey=' . $this->hapikey;
+		$this->http($endpoint,$post_json);
+	}
+	
+	function http($endpoint,$post_json){
+
+        $ch = @curl_init();
+        @curl_setopt($ch, CURLOPT_POST, true);
+        @curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
+        @curl_setopt($ch, CURLOPT_URL, $endpoint);
+        @curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        @curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = @curl_exec($ch);
+        $status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $curl_errors = curl_error($ch);
+        @curl_close($ch);
+        return $response . "<hr>";
+       
+	}
+
+}
+
+
+
+
+?> 
